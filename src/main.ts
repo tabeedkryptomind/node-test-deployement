7d812ce2c291611ca1570d5f620bc798b34502d4
Ubuntu <ubuntu@ip-172-31-13-234.us-east-2.compute.internal>
2021-08-03 07:43:53 +0000

    add buildspec .yml

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("listening at 80");
-  await app.listen(80);
+  console.log("listening at 8080");
+  await app.listen(8080);
 }
 bootstrap();
